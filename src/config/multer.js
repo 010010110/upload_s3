cbf8b9db847997422c7ca7d2d66d70865fc54fc6
Carneiro <gabrielarruda08@gmail.com>
2021-01-13 19:52:41 -0300

    separate into folders

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,6 +1,5 @@
 const multer = require("multer");
 const path = require("path");
-const crypto = require("crypto");
 const aws = require("aws-sdk");
 const multerS3 = require("multer-s3");
 
@@ -28,13 +27,8 @@ const storageTypes = {
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: "public-read",
     key: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
-
-        const fileName = `${hash.toString("hex")}-${file.originalname}`;
-
-        cb(null, fileName);
-      });
+      const fileName = `${req.query.numrecolhimento}/${file.originalname}`;
+      cb(null, fileName);
     },
   }),
 };
@@ -48,9 +42,7 @@ module.exports = {
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
       "image/jpeg",
-      "image/pjpeg",
-      "image/png",
-      "image/gif",
+      "image/jpg",
     ];
 
     if (allowedMimes.includes(file.mimetype)) {
